e0a2e9305ee434d816aaa3336b904910e7466cfb
Jack Bartlett <jackbbart@gmail.com>
2021-03-19 12:54:23 -0700

    Changed the text of the button to "Rolling Time!"

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.example.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.text = "Let's Roll!"
+        rollButton.text = "Rolling Time!"
         rollButton.setOnClickListener{
             Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
         }

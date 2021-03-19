d3e37b3e28c276e980ca8ab741582828da8e45ee
Jack Bartlett <jackbbart@gmail.com>
2021-03-19 13:10:56 -0700

    Changed the text of the button to "oh yeah bear"

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.text = "Rolling Time!"
+        rollButton.text = "oh yeah bear"
         rollButton.setOnClickListener{
             Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
         }
